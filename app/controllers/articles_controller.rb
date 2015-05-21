0bbefb82917b25c0e3f78e430a12945103ffa317
perfectsound <duska94@gmail.com>
2015-05-21 18:07:17 +0000

    zrobilismy liste i backa

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,11 @@ class ArticlesController < ApplicationController
     end
     
     def show
-        
+        @article = Article.find(params[:id])
+    end
+    
+    def index
+        @articles = Article.all
     end
     
     private
