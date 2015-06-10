5b66e9ff49dc37229bfa08a1cf61a5c2ce0d220d
perfectsound <duska94@gmail.com>
2015-06-10 18:45:46 +0000

    10.06 -> edit, update, destroy, show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,20 @@
 class ArticlesController < ApplicationController
     def new
+        @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
         
     end
     
     def create
         @article = Article.new(article_params)
-        @article.save
-        
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
     
     def show
@@ -16,6 +23,23 @@ class ArticlesController < ApplicationController
     
     def index
         @articles = Article.all
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+ 
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @articles = Article.find(params[:id])
+        @article.destroy
+        
+        redirect_to articles_path
     end
     
     private
